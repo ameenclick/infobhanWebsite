9e6fa16755522a470ef29204aca1b3692eb9fe4d
ameeninfobhan <developer@infobhan.cloud>
2022-07-05 06:33:08 +0530

    Brand partner content update

---
+++ backend.php
@@ -0,0 +1,54 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$email= $_POST['email'];
+$name= $_POST['name'];
+$subject = $_POST['subject'];
+$body= $_POST['body'];
+$url=$_POST['url'];
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('info@infobhan.net', 'Infobhan Systems');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo($email, $name);
+    $mail->addCC('abdulsalam@infobhan.net');
+    $mail->addBCC('developer@infobhan.net');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $body."\n Website:".$url;
+    $mail->AltBody = $body;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
