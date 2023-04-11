673c53812629945aeed49ea41f7e61da2e9f1db3
ameeninfobhan <developer@infobhan.cloud>
2023-04-11 13:39:15 +0530

    Php mailer inactive

--- backend.php
+++ backend.php
@@ -21,44 +21,46 @@ $subject = $_POST['subject'];
 $phone=$_POST['phone'];
 $body= $_POST['body'];
 
-try {
-    $env = parse_ini_file('.env');
-    //Server settings
-    $mail->isSMTP();                                      
-    $mail->SMTPDebug = 0;  
-    $mail->SMTPAuth = false;
-    $mail->SMTPSecure = 'none';
-    $mail->Host = "https://a2plcpnl0443.prod.iad2.secureserver.net"; 
-    $mail->Port = 25;
-    $mail->IsHTML(true); 
+echo "Sorry this service tempararly out of order";
 
-    $mail->Username="website@infobhan.net";
-    $mail->Password=$env["EMAIL_PASSWORD"];
-    //Recipients
-    $mail->setFrom('website@infobhan.net');
-    $mail->addAddress('developer@infobhan.net', 'Infobhan Systems');     //Add a recipient
-    //$mail->addBCC('developer@infobhan.net');
+// try {
+//     $env = parse_ini_file('.env');
+//     //Server settings
+//     $mail->isSMTP();                                      
+//     $mail->SMTPDebug = 0;  
+//     $mail->SMTPAuth = false;
+//     $mail->SMTPSecure = 'none';
+//     $mail->Host = "https://a2plcpnl0443.prod.iad2.secureserver.net"; 
+//     $mail->Port = 25;
+//     $mail->IsHTML(true); 
 
-    //Content                                 //Set email format to HTML
-    $mail->Subject = $subject;
-    $mail->Body    ="<br/> Name: <b>".$name."</b><br/> Email: <b>".$email."</b><br/>".$body;
-    $mail->AltBody = $body;
+//     $mail->Username="website@infobhan.net";
+//     $mail->Password=$env["EMAIL_PASSWORD"];
+//     //Recipients
+//     $mail->setFrom('website@infobhan.net');
+//     $mail->addAddress('developer@infobhan.net', 'Infobhan Systems');     //Add a recipient
+//     //$mail->addBCC('developer@infobhan.net');
 
-    $mail->send();
-    ob_end_clean();
-    // Get the root URL dynamically
-    $filename = basename($_SERVER['PHP_SELF']);
-    $request_uri = str_replace($filename, '', $_SERVER['REQUEST_URI']);
-    $root_url = "http" . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "s" : "") . "://";
-    $root_url .= $_SERVER['HTTP_HOST'] . $request_uri;
+//     //Content                                 //Set email format to HTML
+//     $mail->Subject = $subject;
+//     $mail->Body    ="<br/> Name: <b>".$name."</b><br/> Email: <b>".$email."</b><br/>".$body;
+//     $mail->AltBody = $body;
 
-    // Construct the new page URL
-    $new_page_url = $root_url."success.html";
+//     $mail->send();
+//     ob_end_clean();
+//     // Get the root URL dynamically
+//     $filename = basename($_SERVER['PHP_SELF']);
+//     $request_uri = str_replace($filename, '', $_SERVER['REQUEST_URI']);
+//     $root_url = "http" . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "s" : "") . "://";
+//     $root_url .= $_SERVER['HTTP_HOST'] . $request_uri;
 
-    header("Location: $new_page_url");
-} catch (Exception $e) {
-    echo "Message could not be sent.".$e;
-    //echo "\n Mailer Error: {$mail->ErrorInfo}";
-}
+//     // Construct the new page URL
+//     $new_page_url = $root_url."success.html";
+
+//     header("Location: $new_page_url");
+// } catch (Exception $e) {
+//     echo "Message could not be sent.".$e;
+//     //echo "\n Mailer Error: {$mail->ErrorInfo}";
+// }
 
 ?>
